b60300a438f5028e6f277a9f83892e14b181d8ad
Aygün ÖZGÜR <aygunozmen@gmail.com>
2023-09-27 11:25:04 +0300

    Update AndroidSetup.cs

--- Editor/AndroidSetup.cs
+++ Editor/AndroidSetup.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace ZenoxZX.Core.Editor
 {
-    #if UNITY_ANDROID && UNITY_EDITOR
+#if UNITY_ANDROID && UNITY_EDITOR
     
     [InitializeOnLoad]
     public class AndroidSetup
@@ -14,21 +14,7 @@ namespace ZenoxZX.Core.Editor
 
         public static void Set()
         {
-            PlayerSettings.keystorePass = "myPwd";
-            PlayerSettings.keyaliasPass = "myAliasPwd";
-            string keyStore = $"{PlayerSettings.productName}.keystore";
-            string s = Application.dataPath + "/" + keyStore;
-            if (!System.IO.File.Exists(s))
-            {
-                Debug.LogError("Key Store not found. Please create a keystore file in the project root folder. Name it : " + keyStore);
-                Debug.LogError("at : " + s);
-            }
 
-            else
-            {
-                PlayerSettings.Android.keystoreName = Application.dataPath + "/user.keystore";
-            }
-            
         }
     }
 #endif
